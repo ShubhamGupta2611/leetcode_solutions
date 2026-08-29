96534b1fcfb9dcab8953caacb081d0bc30e1ceb0
ShubhamGupta2611 <shubham.gupta.2611.cse@gmail.com>
2026-08-30 01:05:21 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.35%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode* temp=head;int c=1;
+        while(temp->next!=NULL){
+            temp=temp->next;
+            c++;
+        }
+        c=c/2;
+        temp=head;
+        for(int i=0;i<c;i++){
+            temp=temp->next;
+        }
+        return temp;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
